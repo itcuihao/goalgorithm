c0d61b96e91bfa3df002a9e27ef65833a7a5a65a
haoc7 <it_cuiaho@163.com>
2017-12-03 21:59:54 +0800

    shellsort

---
+++ shellsort.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	s := []int{1, 3, 4, 6, 2, 9, 5, 8, 7}
+	shellsort(s)
+	fmt.Println(s)
+}
+
+func shellsort(items []int) {
+	var (
+		n    = len(items)
+		gaps = []int{1}
+		k    = 1
+	)
+
+	for {
+		gap := element(2, k) + 1
+		if gap > n-1 {
+			break
+		}
+		gaps = append([]int{gap}, gaps...)
+		k++
+	}
+
+	for _, gap := range gaps {
+		for i := gap; i < n; i += gap {
+			j := i
+			for j > 0 {
+				if items[j-gap] > items[j] {
+					items[j-gap], items[j] = items[j], items[j-gap]
+				}
+				j = j - gap
+			}
+		}
+	}
+}
+
+func element(a, b int) int {
+	e := 1
+	for b > 0 {
+		if b&1 != 0 {
+			e *= a
+		}
+		b >>= 1
+		a *= a
+	}
+	return e
+}
