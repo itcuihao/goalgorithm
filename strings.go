16dcc611743964a300f891023e7931293561e5ad
haoc7 <it_cuihao@163.com>
2018-01-02 22:59:22 +0800

    strings

--- strings.go
+++ strings.go
@@ -1,26 +1,41 @@
 package main
- 
-import (
-  "fmt"
-  "strings"
-)
- 
-func main() {
-  fmt.Println(strings.Compare("A", "B"))  // A < B
-  fmt.Println(strings.Compare("B", "A"))  // B > A  
-  fmt.Println(strings.Compare("Japan", "Australia"))  // J > A
-  fmt.Println(strings.Compare("Australia", "Japan"))  // A < J
-  fmt.Println(strings.Compare("Germany", "Germany"))  // G == G
-  fmt.Println(strings.Compare("Germany", "GERMANY"))  // GERMANY > Germany
-  fmt.Println(strings.Compare("", ""))
-  fmt.Println(strings.Compare("", " ")) // Space is less
 
-  fmt.Println(strings.Contains("Australia", "Aus"))   // Any part of string
-  fmt.Println(strings.Contains("Australia", "Australian")) 
-  fmt.Println(strings.Contains("Japan", "JAPAN")) // Case sensitive
-  fmt.Println(strings.Contains("Japan", "JAP")) // Case sensitive
-  fmt.Println(strings.Contains("Become inspired to travel to Australia.", "Australia"))
-  fmt.Println(strings.Contains("", ""))
-  fmt.Println(strings.Contains("  ", " ")) // space also consider as string
-  fmt.Println(strings.Contains("12554", "1"))   
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	fmt.Println("---Compare---")
+	fmt.Println(strings.Compare("A", "B"))             // A < B
+	fmt.Println(strings.Compare("B", "A"))             // B > A
+	fmt.Println(strings.Compare("Japan", "Australia")) // J > A
+	fmt.Println(strings.Compare("Australia", "Japan")) // A < J
+	fmt.Println(strings.Compare("Germany", "Germany")) // G == G
+	fmt.Println(strings.Compare("Germany", "GERMANY")) // GERMANY > Germany
+	fmt.Println(strings.Compare("", ""))
+	fmt.Println(strings.Compare("", " ")) // Space is less
+
+	fmt.Println("---Contains---")
+	fmt.Println(strings.Contains("Australia", "Aus")) // Any part of string
+	fmt.Println(strings.Contains("Australia", "Australian"))
+	fmt.Println(strings.Contains("Japan", "JAPAN")) // Case sensitive
+	fmt.Println(strings.Contains("Japan", "JAP"))   // Case sensitive
+	fmt.Println(strings.Contains("Become inspired to travel to Australia.", "Australia"))
+	fmt.Println(strings.Contains("", ""))
+	fmt.Println(strings.Contains("  ", " ")) // space also consider as string
+	fmt.Println(strings.Contains("12554", "1"))
+
+	fmt.Println("---ContainsAny---")
+	fmt.Println(strings.ContainsAny("Australia", "a"))
+	fmt.Println(strings.ContainsAny("Australia", "r & a"))
+	fmt.Println(strings.ContainsAny("JAPAN", "j"))
+	fmt.Println(strings.ContainsAny("JAPAN", "J"))
+	fmt.Println(strings.ContainsAny("JAPAN", "JAPAN"))
+	fmt.Println(strings.ContainsAny("JAPAN", "japan"))
+	fmt.Println(strings.ContainsAny("Shell-12541", "1"))
+
+	//  Contains vs ContainsAny
+	fmt.Println(strings.ContainsAny("Shell-12541", "1-2")) // true
+	fmt.Println(strings.Contains("Shell-12541", "1-2"))    // false
 }
