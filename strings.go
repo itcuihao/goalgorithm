bdf7e482233ca1dd377974ce964b5ee2cd7a769c
haoc7 <it_cuihao@163.com>
2018-01-05 22:12:57 +0800

    string

--- strings.go
+++ strings.go
@@ -52,18 +52,24 @@ func main() {
 		return !unicode.IsLetter(c)
 	}
 	strArray := strings.FieldsFunc(`Australia major cities – Sydney, Brisbane,
-									 Melbourne, Perth, Adelaide – are coastal`, x)
-	for _, v := range strArray {
-		fmt.Println(v)
-	}
+		Melbourne, Perth, Adelaide – are coastal`, x)
+		for _, v := range strArray {
+			fmt.Println(v)
+		}
+		
+		fmt.Println("\n*****************Split by number*******************\n")
 
-	fmt.Println("\n*****************Split by number*******************\n")
-
-	y := func(c rune) bool {
-		return unicode.IsNumber(c)
-	}
+		y := func(c rune) bool {
+			return unicode.IsNumber(c)
+		}
 	testff := strings.FieldsFunc(`1 Sydney Opera House.2 Great Barrier Reef.3 Uluru-Kata Tjuta National Park.4 Sydney Harbour Bridge.5 Blue Mountains National Park.6 Melbourne.7 Bondi Beach`, y)
 	for _, w := range testff {
 		fmt.Println(w)
 	}
+	
+	fmt.Println("---HasPrefix---")
+	fmt.Println(strings.HasPrefix("Australia", "Aus"))
+	fmt.Println(strings.HasPrefix("Australia", "aus"))
+	fmt.Println(strings.HasPrefix("Australia", "Jap"))
+	fmt.Println(strings.HasPrefix("Australia", ""))
 }
