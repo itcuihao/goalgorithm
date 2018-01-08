ea1aedfd08551ebeb4585c655913831f8650f195
haoc7 <it_cuihao@163.com>
2018-01-08 22:31:28 +0800

    string

--- strings.go
+++ strings.go
@@ -53,23 +53,30 @@ func main() {
 	}
 	strArray := strings.FieldsFunc(`Australia major cities – Sydney, Brisbane,
 		Melbourne, Perth, Adelaide – are coastal`, x)
-		for _, v := range strArray {
-			fmt.Println(v)
-		}
-		
-		fmt.Println("\n*****************Split by number*******************\n")
+	for _, v := range strArray {
+		fmt.Println(v)
+	}
 
-		y := func(c rune) bool {
-			return unicode.IsNumber(c)
-		}
+	fmt.Println("\n*****************Split by number*******************\n")
+
+	y := func(c rune) bool {
+		return unicode.IsNumber(c)
+	}
 	testff := strings.FieldsFunc(`1 Sydney Opera House.2 Great Barrier Reef.3 Uluru-Kata Tjuta National Park.4 Sydney Harbour Bridge.5 Blue Mountains National Park.6 Melbourne.7 Bondi Beach`, y)
 	for _, w := range testff {
 		fmt.Println(w)
 	}
-	
+
 	fmt.Println("---HasPrefix---")
 	fmt.Println(strings.HasPrefix("Australia", "Aus"))
 	fmt.Println(strings.HasPrefix("Australia", "aus"))
 	fmt.Println(strings.HasPrefix("Australia", "Jap"))
 	fmt.Println(strings.HasPrefix("Australia", ""))
+
+	fmt.Println("---HasSuffix---")
+	fmt.Println(strings.HasSuffix("Australia", "lia"))
+	fmt.Println(strings.HasSuffix("Australia", "A"))
+	fmt.Println(strings.HasSuffix("Australia", "LIA"))
+	fmt.Println(strings.HasSuffix("123456", "456"))
+	fmt.Println(strings.HasSuffix("Australia", ""))
 }
