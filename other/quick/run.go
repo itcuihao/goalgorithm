71466eb0feb1306bf86f77616a4991714e4e4220
itcuihao <it_cuihao@163.com>
2018-12-23 22:39:11 +0800

    三向切分的快速排序

--- other/quick/run.go
+++ other/quick/run.go
@@ -1,1 +1,27 @@
 package quick
+
+func quick3way(a []int, lo, hi int) {
+	if lo >= hi {
+		return
+	}
+
+	lt, i, gt := lo, lo+1, hi
+
+	v := a[lo]
+
+	for i <= gt {
+		if a[i] < v {
+			a[lt], a[i] = a[i], a[lt]
+			lt++
+			i++
+		} else if a[i] > v {
+			a[gt], a[i] = a[i], a[gt]
+			gt--
+		} else {
+			i++
+		}
+	}
+
+	quick3way(a, lo, lt-1)
+	quick3way(a, gt+1, hi)
+}
--- other/quick/run_test.go
+++ other/quick/run_test.go
@@ -1,1 +1,13 @@
 package quick
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestQuick3(t *testing.T) {
+	a := []int{2, 1, 3, 5, 4}
+
+	quick3way(a, 0, 4)
+	fmt.Println(a)
+}
