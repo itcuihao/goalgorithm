877b38fc32b2c2df2dd4f7bf7601e33c27ca3dbd
itcuihao <it_cuihao@163.com>
2021-04-26 08:43:55 +0800

    update

--- other/resume/tt/run.go
+++ other/resume/tt/run.go
@@ -1,5 +1,7 @@
 package tt
 
+import "fmt"
+
 // 无序数组的唯一元素
 // 相关题目 leetcode 136 540
 
@@ -14,3 +16,45 @@ package tt
 // 输入: [4,4,2,3,3,1,1,5,5,9,9,6,6]，输出: 2
 // 输入: [4,4,2,2,1,1,5,5,9,9,3,6,6]，输出: 3
 // 输入: [1,1,8,8,4,4,5,5,6,6,7,7,3,3,2]，输出: 2
+
+func run() {
+	nums := []int{4, 4, 2, 2, 3, 1, 1, 5, 5, 9, 9, 6, 6}
+	i := search(nums)
+	fmt.Println(i)
+}
+func search(nums []int) int {
+	if len(nums) == 0 {
+		return -1
+	}
+	if len(nums) == 1 {
+		return nums[0]
+	}
+	l := len(nums)
+	s, e := 0, l
+	for s < e {
+		m := (s + e) / 2
+
+		n1 := nums[s:m]
+		n1l := len(n1)
+		n2 := nums[m:]
+		n2l := len(n2)
+		if nums[m] != nums[m-1] && nums[m] != nums[m+1] {
+			return nums[m]
+		}
+		if nums[m] == nums[m-1] {
+			if n1l%2 == 0 {
+				s = m
+			} else {
+				e = m - 1
+			}
+		}
+		if nums[m] == nums[m+1] {
+			if n2l%2 == 0 {
+				s = m + 1
+			} else {
+				s = m
+			}
+		}
+	}
+	return -1
+}
