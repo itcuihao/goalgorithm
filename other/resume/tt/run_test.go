4ad9def98b627d5d16a4e048610cc93d01e88c93
itcuihao <it_cuihao@163.com>
2021-05-22 23:47:27 +0800

    update

--- other/resume/tt/run_test.go
+++ other/resume/tt/run_test.go
@@ -1,11 +1,26 @@
 package tt
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 //func TestRun(t *testing.T) {
 //	run()
 //}
 
 func TestRunF(t *testing.T) {
-	funcf()
+	//funcf()
+	var flist []func()
+	for i := 0; i < 3; i++ {
+		i := i
+		flist = append(flist, func() {
+			fmt.Printf("%p", &i)
+			// 3 3 3
+			println(i)
+		})
+		for j := 0; j < 3; j++ {
+			fmt.Printf("%p", flist[j])
+		}
+	}
 }
